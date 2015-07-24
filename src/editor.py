2b8e42d7ee51f372bb6188522ae79a5a1cc33d70
William Emfinger <waemfinger@gmail.com>
2015-07-23 21:39:43 -0500

    Updated towards better functioning api.

--- src/editor.py
+++ src/editor.py
@@ -30,16 +30,59 @@ class Editor(QtGui.QMainWindow):
 
     def init_ui(self):
 
-        exitAction = Action('exit.png', '&Exit', self)        
+        exitAction = Action('icons/toolbar/stop.png', '&Exit', self)        
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(QtGui.qApp.quit)
 
-        self.menubar = MenuBar(self)
-        self.menubar.add_menu('&File')
-        self.menubar.add_action('&File',exitAction)
+        self.menus = {}
+        self.menubar = self.menuBar()
+        self.menubar_add_menu('&File')
+        self.menu_add_action('&File',exitAction)
+
+        self.toolbars = {}
+        self.toolbar_create("test1")
+        self.toolbar_add_action("test1",exitAction)
+        self.toolbar_create("test2")
+        self.toolbar_add_action("test2",exitAction)
 
         self.show()
+
+    def menubar_add_menu(self, menu_name):
+        """
+        :param in string menu_name: name of the menu, shortcut (denoted by &) optional (note that the & will be stripped)
+        """
+        dict_name = menu_name.strip('&')
+        if dict_name not in self.menus:
+            self.menus[dict_name] = self.menubar.addMenu(menu_name)
+
+    def menu_add_action(self, menu_name, menu_action):
+        """
+        :param in string menu_name: name used to create the menu from :func:`Editor.menubar_add_menu`.
+        :param in Action menu_action: :class:`Action.Action` object to bind to the menu.
+        """
+        dict_name = menu_name.strip('&')
+        if dict_name in self.menus:
+            self.menus[dict_name].addAction(menu_action)
+
+
+    def toolbar_create(self, tb_name):
+        """
+        :param in string tb_name: name of the toolbar
+        """
+        if tb_name not in self.toolbars:
+            self.toolbars[tb_name] = self.addToolBar(tb_name)
+
+    def toolbar_add_action(self, tb_name, tb_action):
+        """
+        :param in string tb_name: name used to create the toolbar from :func:`Editor.toolbar_create`.
+        :param in Action tb_action: :class:`Action.Action` object to bind to the toolbar.
+        """
+        if tb_name in self.toolbars:
+            self.toolbars[tb_name].addAction(tb_action)
+
+    def toolbar_remove(self, tb_name):
+        pass
 
 if __name__ == "__main__":
     import sys
