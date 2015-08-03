56e4283cedbd45a8c1b322c0361d88aacc8a4d9a
William Emfinger <emfinger@isis.vanderbilt.edu>
2015-08-02 23:37:43 -0500

    Further update for getting layout and (re-)parenting working

--- src/editor_widget.py
+++ src/editor_widget.py
@@ -18,37 +18,110 @@ from PyQt4 import QtGui
 # IMPLEMNET DRAW STYLING
 
 class EditorItem(QtGui.QGraphicsWidget):
-    def __init__(self, pixmap, parent = None, layout = 'horizontal'):
+    def __init__(self,
+                 parent = None,
+                 image_file = "",
+                 anchor = "top left",
+                 width = 40,
+                 height = 40,
+                 layout = 'horizontal'):
         super(EditorItem, self).__init__(parent)
 
+        self.image_file = image_file
+        self.layout_style = layout
+        self.item = None
+        self.anchor = anchor
+        self.width = width
+        self.height = height
+
+        self.loadResources()
+        
+    def loadResources(self):
+        self.resize(self.width, self.height)
         self.initializeFlags()
         self.setAcceptDrops(True)
         self.setAcceptHoverEvents(True)
-
-        self.item = QtGui.QGraphicsPixmapItem()
-        self.item.setPixmap(pixmap)
-        self.resize(QtCore.QSizeF(pixmap.size()))
-        self.updateGeometry()
         
         child_layout = None
-        if 'horizontal' in layout:
+        if 'horizontal' in self.layout_style:
             child_layout = QtGui.QGraphicsLinearLayout()
-        elif 'vertical' in layout:
+        elif 'vertical' in self.layout_style:
             child_layout = QtGui.QGraphicsLinearLayout(QtCore.Qt.Vertical)
-        elif 'grid' in layout:
+        elif 'grid' in self.layout_style:
             child_layout = QtGui.QGraphicsGridLayout()
+
         self.setLayout(child_layout)
 
+        if self.image_file:
+            self.item = QtGui.QGraphicsPixmapItem()
+            self.item.setPixmap(QtGui.QPixmap(self.image_file))
+            self.resize(QtCore.QSizeF(self.item.pixmap().size()))
+
+        self.setCursor(QtCore.Qt.OpenHandCursor)
+
+    def sizeHint(self, which, constraint):
+        if self.item:
+            return self.item.boundingRect().size()
+        elif self.layout():
+            return self.layout().sizeHint(which, constraint)
+        else:
+            return QtCore.QSizeF(50,50)
+        
+    def updateGraphicsItem(self):
+        self.item.setPixmap(
+            self.item.pixmap().scaled(
+                self.layout().sizeHint(QtCore.Qt.SizeHint(), QtCore.QSizeF()).width(),
+                self.layout().sizeHint(QtCore.Qt.SizeHint(), QtCore.QSizeF()).height()
+            )
+        )
+
+    def removeChild(self, child):
+        self.layout().removeItem(child)
+        self.layout().activate()
+        self.updateGraphicsItem()
+
     def addChild(self, child):
-        self.item.prepareGeometryChange()
-        child.setParentLayoutItem(self)
         self.layout().addItem(child)
-        self.updateGeometry()
+        self.layout().invalidate()
+        self.updateGraphicsItem()
+
+    def parentEditorItem(self):
+        currentParent = self.parentLayoutItem()
+        if currentParent:
+            currentParent = currentParent.parentLayoutItem()
+        return currentParent
 
     def paint(self, painter, option, widget = None):
         self.item.paint(painter, option, widget)
+    
+    def mousePressEvent(self, event):
+        if self.shape().contains(event.pos()):
+            QtGui.QGraphicsWidget.mousePressEvent(self, event)
+            self.dragPos = self.mapFromScene(event.scenePos())
+            self.validDrag = True
+        else:
+            self.validDrag = False
 
-    # FUNCTIONS I NEED FOR EDITOR
+    def mouseReleaseEvent(self, event):
+        if self.validDrag:
+            self.validDrag = False
+            newParent = [x for x in self.scene().items(event.scenePos()) if x != self]
+            currentParent = self.parentEditorItem()
+            if newParent:
+                if newParent[0] != currentParent:
+                    if currentParent: currentParent.removeChild(self)
+                    newParent[0].addChild(self)
+                    self.setPos(newParent[0].mapFromScene(event.scenePos()) - self.dragPos)
+            else:
+                if currentParent:
+                    currentParent.removeChild(self)
+                    self.setParentItem(None)
+                    self.setParent(self.scene())
+                    self.setPos(event.scenePos() - self.dragPos)
+            QtGui.QGraphicsWidget.mouseReleaseEvent(self, event)
+        else:
+            event.ignore()
+
     def initializeFlags(self):
         self.setFlag(QtGui.QGraphicsItem.ItemIsMovable)
         self.setFlag(QtGui.QGraphicsItem.ItemIsSelectable)
@@ -101,15 +174,25 @@ class EditorView(QtGui.QGraphicsView):
 
         scene = EditorScene(self)
 
-        pixmap = QtGui.QPixmap('icons/toolbar/build.png')
-        r = EditorItem(pixmap)
-        t = EditorItem(pixmap.scaled(pixmap.size() / 2))
-        r.addChild(t)
-        t = EditorItem(pixmap.scaled(pixmap.size() / 2))
-        r.addChild(t)
-        t = EditorItem(pixmap.scaled(pixmap.size() / 2))
-        r.addChild(t)
+        icon_file = 'icons/toolbar/terminal.png'
+
+        r = EditorItem(image_file = icon_file, layout='vertical')
         scene.addItem(r)
+
+        icon_file = 'icons/toolbar/build.png'
+
+        t = EditorItem(image_file = icon_file)
+        scene.addItem(t)
+        r.addChild(t)
+
+        t = EditorItem(image_file = icon_file)
+        scene.addItem(t)
+        r.addChild(t)
+
+        t = EditorItem(image_file = icon_file)
+        scene.addItem(t)
+        r.addChild(t)
+
 
         self.setScene(scene)
         self.show()
@@ -124,7 +207,6 @@ class EditorView(QtGui.QGraphicsView):
             self.setDragMode(QtGui.QGraphicsView.RubberBandDrag)
         QtGui.QGraphicsView.keyReleaseEvent(self, event)
 
-
 class TabbedEditor(QtGui.QTabWidget):
     def __init__(self, parent):
         super(TabbedEditor, self).__init__(parent)
@@ -136,8 +218,8 @@ class TabbedEditor(QtGui.QTabWidget):
 
         newPage = EditorView(self)
         self.addTab(newPage, "test1")
-        #newPage = EditorView(self)
-        #self.addTab(newPage, "test2")
-        
+        newPage = EditorView(self)
+        self.addTab(newPage, "test2")
+       
     def onTabClose(self,index):
         self.removeTab(index)
