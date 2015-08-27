bab6e4ce7645e2d2583bb1023a0c8f465ec3b887
William Emfinger <waemfinger@gmail.com>
2015-08-27 08:58:29 -0500

    updated to handle display of attribute classes

--- src/model_tree.py
+++ src/model_tree.py
@@ -9,40 +9,25 @@ from PyQt4 import QtGui
 
 from classes import *
 
-p = Project("test project")
-p.add_child(Software("software"))
-p.children[-1].add_child(Package("Package 1"))
-p.children[-1].children[-1].add_child(Message("Message 1", "int8 test"))
-p.children[-1].children[-1].add_child(Message("Service 1", "int8 test"))
-p.children[-1].children[-1].add_child(Message("Service 2", "int8 test"))
-p.children[-1].add_child(Package("Package 2"))
-p.children[-1].children[-1].add_child(Message("Message 2", "int8 test"))
-p.children[-1].children[-1].add_child(Message("Message 3", "int8 test"))
-p.children[-1].add_child(Package("Package 3"))
-p.children[-1].children[-1].add_child(Message("Message 4", "int8 test"))
-p.children[-1].add_child(Package("Package 4"))
-p.children[-1].children[-1].add_child(Message("Message 5", "int8 test"))
-p.add_child(Hardware("hardware"))
-p.add_child(Deployment("deployment"))
-
 class ModelTree(QtGui.QTreeWidget):
     def __init__(self,parent):
         super(ModelTree,self).__init__(parent)
         self.setColumnCount(2)
         
-    def populate(self, model = p, parent = None):
+    def populate(self, model = project, parent = None):
         if parent:
             item = QtGui.QTreeWidgetItem(parent)
         else:
             item = QtGui.QTreeWidgetItem()
             self.addTopLevelItem(item)
         for key,attr in model.attributes.iteritems():
-            if "name" in key:
-                item.setText(0, attr)
-            else:
-                attrItem = QtGui.QTreeWidgetItem(item)
-                attrItem.setText(0, key)
-                attrItem.setText(1, attr)
+            if attr:
+                if "name" in key:
+                    item.setText(0, str(attr.value))
+                else:
+                    attrItem = QtGui.QTreeWidgetItem(item)
+                    attrItem.setText(0, key)
+                    attrItem.setText(1, str(attr.value))
         for child in model.children:
             self.populate(child, item)    
 
