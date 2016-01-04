0384564fbd5aed4c43c4bed86ce84d00ce3a8334
William Emfinger <emfinger@isis.vanderbilt.edu>
2016-01-04 06:21:52 -0800

    minor update to test model.

--- src/test_model.py
+++ src/test_model.py
@@ -51,3 +51,31 @@ c7.add_child(c9)
 c7.add_child(c10)
 c7.add_child(c11)
 c7.add_child(c12)
+
+c13 = Model()
+c13['Name'].value = 'Host'
+c14 = Model()
+c14['Name'].value = 'Route'
+
+c2.add_child(c13)
+c2.add_child(c14)
+
+c15 = Model()
+c15['Name'].value = 'Interface'
+
+c13.add_child(c15)
+
+c16 = Model()
+c16['Name'].value = 'Link'
+
+c14.add_child(c16)
+
+c17 = Model()
+c17['Name'].value = 'Node'
+
+c3.add_child(c17)
+
+c18 = Model()
+c18['Name'].value = 'Component Instance'
+
+c17.add_child(c18)
